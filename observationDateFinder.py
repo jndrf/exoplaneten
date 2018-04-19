466f920a14e70d74c70c1565e0313f3ea5fc2fae
Jonas Neundorf <JNeun1@web.de>
2018-04-19 17:50:01 +0200

    finding transit dates, first date may be out of specified range

--- observationDateFinder.py
+++ observationDateFinder.py
@@ -28,6 +28,38 @@ Might show deviations in the last shown digit due to numerical artifacts
 
     return round(int(365.25*(year+4716)) + int(30.6001*(month+1)) + day + time + b - 1524.5, 5)
 
+def findTransitDate(refValue, period, calendaric=False, **kwargs):
+    '''
+Gives times of planet transits as list (julian date)
+@param revValue: reference transit date (julian)
+@param period: orbital period (days)
+
+optional parameters
+@param calendaric: return list of calendar dates if true
+@param start: start of observation period (julian date) use cstart for calendar date
+@param end: end of observation period, cend for calendar date
+@param n_transits: number of transits to show
+
+    '''
+    if 'start' in kwargs:
+        n_min = (kwargs['start'] - revValue)//period
+    elif 'cstart' in kwargs:
+        n_min = (calendarToJulian(kwargs['cstart']) - revValue)//period
+    else:
+        n_min = 0
+
+    if 'n_transits' in kwargs:
+        n_transits = kwargs['n_transits']
+    elif 'end' in kwargs:
+        n_max = (kwargs['end'] - revValue)//period
+    elif 'cend' in kwargs:
+        n_max = (calendarToJulian(kwargs['cend']) - revValue)//period
+    else:
+        print('either n_transits, end or cend have to be given')
+        assert(False)
+
+    transitdates = [revValue+ i*period for i in range(n_min, n_transits+1)]
 
 if __name__ == '__main__':
     print(calendarToJulian('2018-04-19T15:08:01'))
+    print(transitdates(2453957.635486, 2.470613402, cstart='2018-05-07', cend='2018-05-21')
