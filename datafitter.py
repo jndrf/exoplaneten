dc302b88b04d94ab51b64fe5622184c790641a22
Jonas Neundorf <jneun1@web.de>
2018-08-13 12:00:03 +0200

    chisquare, formatting

--- datafitter.py
+++ datafitter.py
@@ -28,13 +28,44 @@ still wont fit it.
 intab = read_csv('Measurements.csv')
 
 timelist = intab.JD_UTC.subtract(58253)
-params = curve_fit(transitModel, timelist, intab.rel_flux_T1, sigma=intab.rel_flux_err_T1, p0=[25, 27, 0.92, 0.95, .5], bounds=([24, 25, 0.91, 0.92, -np.inf], [27, 28, 0.94, 0.96, np.inf]))
+
+params = curve_fit(transitModel,
+                   timelist,
+                   intab.rel_flux_T1,
+                   sigma=intab.rel_flux_err_T1,
+                   p0=[25, 27, 0.92, 0.95, .5],
+                   bounds=(
+                       [24, 25, 0.91, 0.92, -np.inf],
+                       [27, 28, 0.94, 0.96, np.inf]
+                   ),
+                   absolute_sigma=True)
 
 print(params[0])
 print(params[0][3])
 covmat = params[1]
 deptherror = np.sqrt(covmat[2][2]*params[0][3]**-2 + params[0][2]**2 / (params[0][3]**4)*covmat[3][3])
-print('transit depth\t {depth:.3} +- {de:.3}\n3rd Contact\t {t3:.4} +- {e3:.3}\n4th Contact\t {t4:.4} +- {e4:.3}'.format(depth=1-params[0][0]/params[0][1], t3=params[0][2], t4=params[0][3], de=deptherror, e3=np.sqrt(covmat[2][2]), e4=np.sqrt(covmat[3][3])))
+
+# calculate chisquare
+residuals = intab.rel_flux_T1 - transitModel(timelist, *params[0])
+chisquare = np.sum(residuals**2 / intab.rel_flux_err_T1**2)
+
+print('''
+transit depth\t {depth:.3} +- {de:.3}
+3rd Contact\t {t3:.4} +- {e3:.3}
+4th Contact\t {t4:.4} +- {e4:.3}
+Chi^2\t\t {chi2}
+N_dof\t\t {ndof}
+'''.format(depth=1-params[0][0]/params[0][1],
+           t3=params[0][2],
+           t4=params[0][3],
+           de=deptherror,
+           e3=np.sqrt(covmat[2][2]),
+           e4=np.sqrt(covmat[3][3]),
+           chi2=chisquare,
+           ndof=(len(intab.rel_flux_T1) - len(params[0]))
+)
+)
+
 time_fine = np.linspace(timelist.iloc[0], timelist.iloc[-1], 10000)
 plt.errorbar(timelist, intab.rel_flux_T1, intab.rel_flux_err_T1, fmt='.')
 plt.plot(time_fine, transitModel(time_fine, *params[0]))
